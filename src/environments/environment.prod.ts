9a2d83e0b98eb6820ad9f37ad88ae4d1ba39606a
waynegakuo <waynejotham@gmail.com>
2022-12-05 14:04:08 +0300

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBg-VRstdzUsV3siGJB8lKziCzPDdzeX38",
+    authDomain: "i18ndemoke.firebaseapp.com",
+    projectId: "i18ndemoke",
+    storageBucket: "i18ndemoke.appspot.com",
+    messagingSenderId: "1008676289683",
+    appId: "1:1008676289683:web:5e3b2f2e4201aeb65d2166",
+    measurementId: "G-PTP2YE41F5"
+  }
+}
