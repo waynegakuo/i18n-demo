00c8305fc5e2a23c7f26b7443e9331228213df71
waynegakuo <waynejotham@gmail.com>
2022-12-05 14:45:15 +0300

    feat(project): i18n in Angular demo complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'i18n-demo';
+
+  siteLanguage: string | undefined = 'English';
+
+  siteLocale!: string;
+
+  languageList = [
+    {code: 'en', label: 'English'},
+    {code: 'esp', label: 'Español'},
+    {code: 'fr', label: 'Français'},
+  ];
+
+  ngOnInit(): void {
+    this.siteLocale = window.location.pathname.split('/')[1];
+    this.siteLanguage = this.languageList.find(f => f.code === this.siteLocale)?.label;
+  }
 }
